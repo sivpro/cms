3a9da5028cd6476633e13c72b7b1d04de4667f35
Innokenty <6319432@gmail.com>
2014-07-15 23:37:45 +0600

    Исправлено регулярное выражение проверки e-mail

--- libs/formvalidator.js
+++ libs/formvalidator.js
@@ -353,7 +353,7 @@ function check_numberandsimbols(field, value, formName) {
 // E-mail validation
 function check_email(field, value, formName) {
 	var string = jQuery("#"+field+"").val(),
-		reg = new RegExp("[0-9a-z_]+@[0-9a-z_^.]+\\.[a-z]{2,3}", 'i'),
+		reg = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/i,
 		result = reg.test(string);
 
 	return result;
