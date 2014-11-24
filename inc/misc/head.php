dba8a852a58e96fa390c33bef8f07f0c4a35223c
Innokenty <6319432@gmail.com>
2014-11-24 12:39:35 +0500

    Add canonical link element to _page pages

--- inc/misc/head.php
+++ inc/misc/head.php
@@ -8,6 +8,10 @@
 		$page->description = $control->descriptionSeo;
 		$page->keywords = $control->keywordsSeo;
 
+		if ($control->page != "") {
+			$page->canonicalUrl = $_SERVER['DOMAIN_NAME'].$control->module_url;
+		}
+
 
 		$text = sprintt($page, 'templates/misc/'.$wrapper);
 		return $text;
